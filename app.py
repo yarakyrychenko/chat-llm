b666a3ac4e8c1b4c6ef9e98c099aff7bd88580dd
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2024-11-12 22:03:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ with right:
     with st.expander("⏹️ End Conversation"):
         st.text_input(label="Enter your Prolific ID",key="user_id")
         st.slider('Rate the conversation from *Terrible* to *Perfect*. There are no right or wrong answers.', 0, 100, format="", key="score", value=50)
-        if st.button('Submit', key=None, help=None, disabled = len(st.session_state.messages) < 2):
+        if st.button('Submit', key=None, help=None, disabled = len(st.session_state.messages) < 2, use_container_width=True):
             submission_time = datetime.now().strftime('%Y%m-%d%H-%M%S')
 
             user_data={"user_id":st.session_state.user_id,
