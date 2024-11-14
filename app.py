cf51d411a659a01db9a18e4fcd14bd8d5ee48f42
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2024-11-14 16:54:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -80,37 +80,6 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-def ask_llm():
-    prompt = st.session_state.prompt
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        try:
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-            st.session_state.messages.append(
-                {"role": "assistant", "content": response}
-            )
-        except:
-            st.session_state.max_messages = len(st.session_state.messages)
-            rate_limit_message = """
-                Oops! Sorry, I can't talk now. Too many people have used
-                this service recently.
-            """
-            st.session_state.messages.append(
-                {"role": "assistant", "content": rate_limit_message}
-            )
-            st.rerun()
-
 if len(st.session_state.messages) >= st.session_state.max_messages:
     st.info(
         """Notice: The maximum message limit for this demo version has been reached. Thank you for your understanding."""
@@ -119,11 +88,7 @@ if len(st.session_state.messages) >= st.session_state.max_messages:
 elif st.query_params['p'] == 't' and st.session_state.user_info == '':
     st.info('Please enter a short summary of your personal circumstances to start the conversation.')
     
-else:
-    left, right = st.columns(2)
-
-    with left:
-        st.chat_input("Ask something...",key='prompt')
+st.chat_input("Ask something...",key='prompt')
 
 if st.session_state.prompt:
     prompt = st.session_state.prompt
@@ -156,26 +121,25 @@ if st.session_state.prompt:
             )
             st.rerun()
 
-    with right:
-        with st.expander("⏹️ End Conversation"):
-            st.text_input(label="Enter your Prolific ID",key="user_id")
-            st.slider('Rate the conversation from *Terrible* to *Perfect*. There are no right or wrong answers.', 0, 100, format="", key="score", value=50)
-            if st.button('Submit', key=None, help=None, disabled = len(st.session_state.messages) < 2, use_container_width=True):
-                submission_time = datetime.now().strftime('%Y%m-%d%H-%M%S')
+with st.expander("⏹️ End Conversation"):
+    st.text_input(label="Enter your Prolific ID",key="user_id")
+    st.slider('Rate the conversation from *Terrible* to *Perfect*. There are no right or wrong answers.', 0, 100, format="", key="score", value=50)
+    if st.button('Submit', key=None, help=None, disabled = len(st.session_state.messages) < 2, use_container_width=True):
+        submission_time = datetime.now().strftime('%Y%m-%d%H-%M%S')
 
-                user_data={"user_id":st.session_state.user_id,
-                            "conversation":st.session_state.messages,
-                            "score":st.session_state.score,
-                            "time":submission_time}
-                
-                from pymongo.mongo_client import MongoClient
-                from pymongo.server_api import ServerApi
-                with MongoClient(st.secrets["mongo"],server_api=ServerApi('1')) as client:
-                        db = client.chat
-                        collection = db.app
-                        collection.insert_one(user_data)  
-                        st.session_state.inserted += 1
+        user_data={"user_id":st.session_state.user_id,
+                    "conversation":st.session_state.messages,
+                    "score":st.session_state.score,
+                    "time":submission_time}
+        
+        from pymongo.mongo_client import MongoClient
+        from pymongo.server_api import ServerApi
+        with MongoClient(st.secrets["mongo"],server_api=ServerApi('1')) as client:
+                db = client.chat
+                collection = db.app
+                collection.insert_one(user_data)  
+                st.session_state.inserted += 1
 
-                        setup_messages()
-                        st.rerun()
+                setup_messages()
+                st.rerun()
     
