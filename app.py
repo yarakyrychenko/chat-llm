a77217b7bed78bc07524de690a0387d2b2474c2e
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2024-11-26 19:08:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if 'inserted' not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o-mini-2024-07-18"
     st.session_state.max_messages = 20
     st.session_state.user_id = ''
-    st.session_state.climate_actions = []
+    st.session_state.climate_actions = ''
     st.session_state.age = ''
     st.session_state.gender = ''
     st.session_state.education = ''
@@ -67,8 +67,7 @@ def setup_messages():
     elif st.query_params["k"] == "f" and st.query_params["p"] == "t":
         st.session_state.system_message = st.session_state.personalization_text.replace('[USER_INFO]',st.session_state.user_info) + '\n\n' + st.session_state.base_text
     else:
-        climate_actions = ", ".join(st.session_state.climate_actions)
-        personalization_text = st.session_state.personalization_text.replace('[AGE]',str(st.session_state.age)).replace('[GENDER]',st.session_state.gender).replace('[EDUCATION]',st.session_state.education).replace('[CLIMATE_ACTIONS]',climate_actions).replace('[LOCALITY]',st.session_state.locality).replace('[PROPERTY]',st.session_state.property).replace('[INCOME]',st.session_state.income).replace('[ZIPCODE]',st.session_state.zipcode)
+        personalization_text = st.session_state.personalization_text.replace('[AGE]',str(st.session_state.age)).replace('[GENDER]',st.session_state.gender).replace('[EDUCATION]',st.session_state.education).replace('[CLIMATE_ACTIONS]',st.session_state.climate_actions).replace('[LOCALITY]',st.session_state.locality).replace('[PROPERTY]',st.session_state.property).replace('[INCOME]',st.session_state.income).replace('[ZIPCODE]',st.session_state.zipcode)
         st.session_state.system_message = st.session_state.knowledge_text + '\n\n' + personalization_text.replace('[USER_INFO]',st.session_state.user_info)  + '\n\n' + st.session_state.base_text
 
     st.session_state.messages = [{ "role": "system", "content": st.session_state.system_message}]
@@ -111,12 +110,11 @@ with st.expander("Form",expanded=not st.session_state.submitted):
                   'Other' ], key="property")
         st.radio("What was your total household income before taxes during the past 12 months?",
                     ['','Less than \$25,000','\$25,000 to \$49,999','\$50,000 to \$74,999','\$75,000 to \$99,999','\$100,000 to \$149,999','\$150,000 or more'], key="income")
-        st.multiselect('What climate actions are you currently taking?',
-                       st.session_state.actions_list,
+        st.text_area('Please describe any actions you are taking to address climate change? Write "None" if you are not taking any.',
                        key="climate_actions"
         )
         st.text_area(
-        "Write at least three sentences about yourself.",
+        "Write at least two sentences about yourself. You can write about your job, hobbies, living arrangements or any other information you think might be relevant. *Do not write anything that could identify you, such as your name or address.*",
         '', key='user_info')
 
         columns_form = st.columns((1,1,1))
